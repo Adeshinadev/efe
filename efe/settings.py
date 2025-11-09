055e062397c00180433eb90bb5efb032bc5af1c7
Adeshina <53658477+Adeshinadev@users.noreply.github.com>
2025-11-10 00:30:37 +0100

    update

--- efe/settings.py
+++ efe/settings.py
@@ -95,7 +95,7 @@ WSGI_APPLICATION = 'efe.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-if not DEBUG:
+if DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
